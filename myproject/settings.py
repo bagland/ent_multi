b6de33ec160098875ff6e9c170eae0ad8c0902a5
b_daribayev <b_daribayev@gmail.com>
2016-10-21 17:05:47 +0600

    auth stuff

--- myproject/settings.py
+++ myproject/settings.py
@@ -118,7 +118,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     )
 }
 
