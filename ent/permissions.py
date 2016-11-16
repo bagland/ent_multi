c4186c5085399c9ba25ef03890b5f8319aff1143
b_daribayev <b_daribayev@gmail.com>
2016-11-16 17:02:18 +0600

    Permissions working

--- ent/permissions.py
+++ ent/permissions.py
@@ -1,14 +1,12 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission
 from .models import Role
 
-class IsPartOfCompany(permissions.BasePermission):
+class IsPartOfCompany(BasePermission):
 	def has_object_permission(self, request, view, obj):
-		print('here')
-		if request.user.is_anonymous():
-			return False
+		# return False
+		# print('here')
 		try:
 			role = Role.objects.get(user=request.user)
-			print(role.company)
 			return role.company == obj.company
 		except Role.DoesNotExist:
 			return False
--- ent/views.py
+++ ent/views.py
@@ -13,8 +13,9 @@ from rest_framework import authentication, permissions, viewsets, filters, statu
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.exceptions import PermissionDenied
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-from .models import Product, Sales, Arrival, Returns, SoldProduct, ReturnedProduct, ArrivedProduct
+from .models import Product, Sales, Arrival, Returns, SoldProduct, ReturnedProduct, ArrivedProduct, Company, Role
 from .serializers import ProductSerializer, SalesSerializer, UserSerializer, ArrivalSerializer, ReturnsSerializer, SoldProductSerializer, ReturnedProductSerializer, ArrivedProductSerializer
 from django.template import loader, Context
 from django.core.mail import EmailMultiAlternatives
@@ -29,7 +30,7 @@ class DefaultsMixin(object):
 	# )
 	permission_classes = (
 		permissions.IsAuthenticated,
-		# IsPartOfCompany,
+		IsPartOfCompany,
 	)
 	paginate_by = 25
 	paginate_by_param = 'page_size'
@@ -50,6 +51,15 @@ class ProductViewSet(DefaultsMixin, viewsets.ModelViewSet):
 	lookup_field = 'barcode'
 	filter_class = ProductFilter
 
+	def list(self, request, *args, **kwargs):
+		try:
+			role = Role.objects.get(user=request.user)
+			self.queryset = Product.objects.filter(company=role.company)
+		except Role.DoesNotExist:
+			# raise PermissionDenied('no products registered for a user')
+			self.queryset = Product.objects.none()
+		return super(ProductViewSet, self).list(request, *args, **kwargs)
+
 class SalesViewSet(DefaultsMixin, viewsets.ModelViewSet):
 	"""
 	Продажа товара
@@ -59,11 +69,29 @@ class SalesViewSet(DefaultsMixin, viewsets.ModelViewSet):
 	filter_class = SalesFilter
 	search_fields = ('date',)
 
+	def list(self, request, *args, **kwargs):
+		try:
+			role = Role.objects.get(user=request.user)
+			self.queryset = Sales.objects.filter(company=role.company)
+		except Role.DoesNotExist:
+			# raise PermissionDenied('no products registered for a user')
+			self.queryset = Sales.objects.none()
+		return super(SalesViewSet, self).list(request, *args, **kwargs)
+
 class SoldProductViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
 	queryset = SoldProduct.objects.order_by('date')
 	serializer_class = SoldProductSerializer
 	filter_class = SoldProductFilter
 	search_fields = ('name', 'date',)
+
+	def list(self, request, *args, **kwargs):
+		try:
+			role = Role.objects.get(user=request.user)
+			self.queryset = SoldProduct.objects.filter(sales__company=role.company)
+		except Role.DoesNotExist:
+			# raise PermissionDenied('no products registered for a user')
+			self.queryset = SoldProduct.objects.none()
+		return super(SoldProductViewSet, self).list(request, *args, **kwargs)
 
 class ReturnsViewSet(DefaultsMixin, viewsets.ModelViewSet):
 	"""
@@ -74,11 +102,29 @@ class ReturnsViewSet(DefaultsMixin, viewsets.ModelViewSet):
 	filter_class = ReturnsFilter
 	search_fields = ('date',)
 
+	def list(self, request, *args, **kwargs):
+		try:
+			role = Role.objects.get(user=request.user)
+			self.queryset = Returns.objects.filter(company=role.company)
+		except Role.DoesNotExist:
+			# raise PermissionDenied('no products registered for a user')
+			self.queryset = Returns.objects.none()
+		return super(ReturnsViewSet, self).list(request, *args, **kwargs)
+
 class ReturnedProductViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
 	queryset = ReturnedProduct.objects.order_by('date')
 	serializer_class = ReturnedProductSerializer
 	filter_class = ReturnedProductFilter
 	search_fields = ('date',)
+
+	def list(self, request, *args, **kwargs):
+		try:
+			role = Role.objects.get(user=request.user)
+			self.queryset = ReturnedProduct.objects.filter(returns__company=role.company)
+		except Role.DoesNotExist:
+			# raise PermissionDenied('no products registered for a user')
+			self.queryset = ReturnedProduct.objects.none()
+		return super(ReturnedProductViewSet, self).list(request, *args, **kwargs)
 
 class ArrivalViewSet(DefaultsMixin, viewsets.ModelViewSet):
 	"""
@@ -90,6 +136,15 @@ class ArrivalViewSet(DefaultsMixin, viewsets.ModelViewSet):
 	filter_class = ArrivalFilter
 	ordering_fields = ('date',)
 
+	def list(self, request, *args, **kwargs):
+		try:
+			role = Role.objects.get(user=request.user)
+			self.queryset = Arrival.objects.filter(company=role.company)
+		except Role.DoesNotExist:
+			# raise PermissionDenied('no products registered for a user')
+			self.queryset = Arrival.objects.none()
+		return super(ArrivalViewSet, self).list(request, *args, **kwargs)
+
 class ArrivedProductViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
 	queryset = ArrivedProduct.objects.order_by('date')
 	serializer_class = ArrivedProductSerializer
@@ -97,27 +152,14 @@ class ArrivedProductViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
 	filter_class = ArrivedProductFilter
 	ordering_fields = ('name','date',)
 
-	# def perform_create(self, serializer):
-	# 	user = self.request.user
-	# 	now = timezone.now()
-	# 	name = self.request.data['name']
-	# 	barcode = self.request.data['barcode']
-	# 	amount = self.request.data['amount']
-	# 	description = self.request.data['description']
-	# 	retail_price = self.request.data['retail_price']
-	# 	product, created = Product.objects.get_or_create(barcode=barcode, owner=user)
-	# 	print(product)
-	# 	if not amount:
-	# 		amount = 0
-	# 	if not retail_price:
-	# 		retail_price = 0
-	# 	product.amount_left += Decimal(amount)
-	# 	product.retail_price = retail_price
-	# 	product.name = name
-	# 	product.description = description
-	# 	product.save()
-	# 	serializer.save(owner=user)
-	# 	return createAPISuccessJsonReponse({})
+	def list(self, request, *args, **kwargs):
+		try:
+			role = Role.objects.get(user=request.user)
+			self.queryset = ArrivedProduct.objects.filter(arrival__company=role.company)
+		except Role.DoesNotExist:
+			# raise PermissionDenied('no products registered for a user')
+			self.queryset = ArrivedProduct.objects.none()
+		return super(ArrivedProductViewSet, self).list(request, *args, **kwargs)
 
 class UserViewSet(viewsets.ModelViewSet):
 	# lookup_field = User.USERNAME_FIELD
