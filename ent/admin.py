622c7eb996e1236aba798052c09e857d18452674
Baglan Daribayev <baglan.daribayev@gmail.com>
2017-07-07 22:50:56 +0600

    Added admin permissions to edit companies' products

--- ent/admin.py
+++ ent/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product
+from .models import Product, Role, Company
 
 class ProductModelAdmin(admin.ModelAdmin):
 	list_display = ('name', 'amount_left', 'retail_price', 'wholesale_price', 'barcode', 'company')
 	list_filter = ['company']
 	search_fields = ['name', 'barcode']
+	actions_on_bottom = True
+
 	class Meta:
 		model = Product
+
+	def get_queryset(self, request):
+		qs = super(ProductModelAdmin, self).get_queryset(request)
+		if request.user.is_superuser:
+			return qs
+		role = Role.objects.get(user=request.user)
+		return qs.filter(company=role.company)
 
 admin.site.register(Product, ProductModelAdmin)
